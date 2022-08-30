afa308d9168293d4e9c798261d3d37245e42b84e
Lucas <lucasgmt@al.insper.edu.br>
2022-08-30 09:06:34 -0300

    lab03 finalizado

---
+++ programs/labs/lab03.h
@@ -0,0 +1,35 @@
+#include "mbed.h"
+#include "crazyflie.h"
+#include "parameters.h"
+
+PwmOut motor1(MOTOR1);
+PwmOut motor2(MOTOR2);
+PwmOut motor3(MOTOR3);
+PwmOut motor4(MOTOR4);
+
+// função para converter velocidade angular em duty cycle do PWM
+float control_motor(float omega)
+{
+    return a2*omega*omega + a1*omega; // função obtida experimentalmente 
+}
+
+int main()
+{
+    motor1.period(1.0/500.0); // frequencia do motor
+    motor2.period(1.0/500.0); // frequencia do motor
+    motor3.period(1.0/500.0); // frequencia do motor
+    motor4.period(1.0/500.0); // frequencia do motor
+
+    motor1 = control_motor(2200.0);
+    motor2 = control_motor(2200.0);
+    motor3 = control_motor(2200.0);
+    motor4 = control_motor(2200.0);
+
+    //motor = 0.1;
+    wait(10);
+
+    motor1 = 0.0;
+    motor2 = 0.0;
+    motor3 = 0.0;
+    motor4 = 0.0;
+}
--- src/utils/parameters.h
+++ src/utils/parameters.h
@@ -6,8 +6,12 @@
 // Physical constants
 const float pi = 3.1416;
 const float g = 9.81;       // m/s^2
-const float a2 = 1.203e-07;
-const float a1 = 2.516e-14;
+
+// constantes PWM = a2omega^2 + a1omega
+const float a2 = 1.203e-07; // lab02
+const float a1 = 2.516e-14; // lab02
+
+const float kl = 1.607e-08; // constante de sustentação (lab03)
 
 // Quadcopter dimensions
 const float m = 30.0e-3;    // kg
