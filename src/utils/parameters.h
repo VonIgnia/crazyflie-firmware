57bb0a162f6bb413f0f7c820fc3d621d30226e7c
Lucas <lucasgmt@al.insper.edu.br>
2022-09-01 08:33:10 -0300

    lab04 finalizado

--- src/utils/parameters.h
+++ src/utils/parameters.h
@@ -11,13 +11,14 @@ const float g = 9.81;       // m/s^2
 const float a2 = 1.203e-07; // lab02
 const float a1 = 2.516e-14; // lab02
 
-const float kl = 1.607e-08; // constante de sustentação (lab03)
+const float kl = 1.607e-08; // constante de sustentação (lab03) -- roll (rolagem)
+const float kd = 1.309e-10; // constante de arrasto (lab04) -- yaw (guinagem)
 
 // Quadcopter dimensions
 const float m = 30.0e-3;    // kg
 const float I_xx = 16.0e-6; // kg.m^2
 const float I_yy = 16.0e-6; // kg.m^2
-const float I_zz = 29.0e-6; // kg.m^2
+const float I_zz = 29.0e-6; // kg.m^2 
 const float l = 33.0e-3;    // m
 
 #endif
